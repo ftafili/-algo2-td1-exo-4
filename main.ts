6f920917994323c3310ab258bfa2e1fea24a3c42
ftafili <68041236+ftafili@users.noreply.github.com>
2020-07-16 16:45:11 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,32 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(posX, posY)
+    posX += -1
+    if (posX < 0) {
+        posX = 4
+        posY += -1
+        if (posY < 0) {
+            posY = 4
+        }
+    }
+    led.plot(posX, posY)
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(posX, posY)
+    posX += 1
+    if (posX > 4) {
+        posX = 0
+        posY += 1
+        if (posY > 4) {
+            posY = 0
+        }
+    }
+    led.plot(posX, posY)
+})
+let posY = 0
+let posX = 0
+posX = 2
+posY = 2
+led.plot(posX, posY)
+basic.forever(function () {
+	
+})
